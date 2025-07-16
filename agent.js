46e69f8915f819091e0ef3ae6bf6f122f02b2230
Tvisha Sharma <tvisha.edu@gmail.com>
2025-07-17 01:05:01 +0530

    feat: enhance personal finance assistant with income tracking and balance calculation functions

--- agent.js
+++ agent.js
@@ -1,21 +1,47 @@
+import readline from 'node:readline/promises';
 import Groq from "groq-sdk";
+
 const expenseDB = []
+const incomeDB  = []
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function callAgent() {
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
   const messages = [
     {
       role: "system",
-      content: `You are Renil, a smart and friendly personal finance assistant. Your role is to help users manage and understand their expenses by answering questions, calculating totals, analyzing spending patterns, and providing suggestions when needed. Today's date is ${new Date().toUTCString()}. Be concise, accurate, and helpful in your responses.`,
+      content: `You are Renil, a smart and friendly personal finance assistant. Your role is to help users manage and understand their expenses by answering questions, calculating totals, analyzing spending patterns, and providing suggestions when needed. Today's date is ${new Date().toUTCString()}. Be concise, accurate, and helpful in your responses.
+      You have access to two functions:
+      1. getTotalExpense: This function calculates the total expense within a specified date range
+      2. addExpense: This function allows users to add a new expense entry to the expense database.
+      3. addIncome: This function allows users to add a new income entry to the income database.
+      4. getTotalIncome: This function calculates the total income within a specified date range
+      5. getMoneyBalance: This function calculates the remaining money balance by subtracting total expenses from total income.
+      When the user asks for t  he total expense, you will call the getTotalExpense function with the appropriate date range.
+      When the user wants to add a new expense, you will call the addExpense function with the name and amount of the expense.`,
     },
   ];
 
-  messages.push({
-    role: "user",
-    content: "Hey I just bought an iPhone for 1,00,000 INR. Can you add this expense to my records?",
-  });
-
+  // for user prompt loop
   while(true){
+    // for agent
+    const question = await rl.question("USER: ");
+
+    if(question === "bye"){
+      break;
+    }
+
+    messages.push({
+      role: "user",
+      content: question,
+    });
+
+    while(true){
     const completion = await groq.chat.completions.create({
       messages: messages,
       model: "llama-3.3-70b-versatile",
@@ -63,6 +89,36 @@ async function callAgent() {
             },
           },
         },
+        {
+          type: "function",
+          function: {
+            name: "addIncome",
+            description:
+              "Add new entry to income database",
+            parameters: {
+              type: "object",
+              properties: {
+                name: {
+                  type: "string",
+                  description: "Name of the income. eg: salary, freelance work",
+                },
+                amount: {
+                  type: "string",
+                  description: "Amount of the income",
+                },
+              },
+              required: ["name", "amount"],
+            },
+          },
+        },
+        {
+          type: "function",
+          function: {
+            name: "getMoneyBalance",
+            description:
+              "get remaining money balance from database",
+          },
+        },
       ],
 
     });
@@ -85,6 +141,10 @@ async function callAgent() {
       }
       else if(functionName === "addExpense"){
         result = addExpense(JSON.parse(functionArgs));
+      }else if(functionName === "addIncome"){
+        result = addIncome(JSON.parse(functionArgs));
+      }else if(functionName === "getTotalIncome"){
+        result = getTotalIncome(JSON.parse(functionArgs));
       }
 
       messages.push({
@@ -94,18 +154,20 @@ async function callAgent() {
       })
 
     }
-    console.log("-----------");
-    console.log("Messages",messages)
-    console.log("-----------");
-    console.log("Expense DB", expenseDB);
-    console.log("-----------");
+    // console.log("-----------");
+    // console.log("Messages",messages)
+    // console.log("-----------");
+    // console.log("Expense DB", expenseDB);
+    // console.log("-----------");
   }
+  }
+  rl.close();
 
 }
 callAgent();
 
 function getTotalExpense({ from, to }) {
-  console.log("calling get total expenses");
+  // console.log("calling get total expenses");
   const expense = expenseDB.reduce((acc, item)=>{
     return acc + item.amount;
   }, 0)
@@ -113,7 +175,22 @@ function getTotalExpense({ from, to }) {
 }
 
 function addExpense({ name, amount }) {
-  console.log("adding expense", name, amount);
+  // console.log("adding expense", name, amount);
   expenseDB.push({name, amount});
   return "added expense successfully";
+}
+
+function addIncome({ name, amount }) {
+  incomeDB.push({name, amount});
+  return "added income successfully";
+}
+
+function getMoneyBalance() {
+  const totalIncome = incomeDB.reduce((acc, item) => {
+    acc + item.amount;
+  }, 0);
+  const totalExpense = expenseDB.reduce((acc, item) => {
+    acc + item.amount;
+  }, 0);
+  return `${totalIncome - totalExpense} INR`;
 }
